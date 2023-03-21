77386a56174d3291ffd030deb1397988ea4e8aad
roket64 <1p41i4y1@gmail.com>
2023-03-21 16:10:10 +0900

    OwO

---
+++ src/congruence.h
@@ -0,0 +1,26 @@
+#ifndef CONGRUENCE_H
+#define CONGRUENCE_H
+
+#include <type_traits>
+
+#include "arithmetics.h"
+
+// Basic congruence operations
+
+namespace integer {
+template <class M, class N>
+constexpr std::common_type_t<M, N> solve(M a, N b, const long long &m) {
+    static_assert(std::is_integral_v<M>,
+                  "integer::solve argument must be integers.");
+    static_assert(std::is_integral_v<N>,
+                  "integer::solve argument must be integers.");
+
+    auto [g, u, v] = integer::gcd(a, m);
+    if (b % g) return 0;
+
+    return u * b / g;
+}
+
+}  // namespace integer
+
+#endif  // congruence.h

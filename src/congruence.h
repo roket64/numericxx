dafcd21a0b9ce5380473f541ae5a13697e8357cd
roket64 <1p41i4y1@gmail.com>
2023-06-19 16:10:50 +0900

    readablitiy & logic fix

--- src/congruence.h
+++ src/congruence.h
@@ -1,29 +1,47 @@
 #ifndef CONGRUENCE_H
 #define CONGRUENCE_H
 
-#include <type_traits>
+#include <vector>
 
 #include "basic_arithmetics.h"
+#include "exceptions.h"
 
 // Basic congruence operations
 
 namespace integer {
-    /**
-     *  @brief Solve congruence ax = b (mod m) of x.
-    */
-template <class M, class N>
-constexpr std::common_type_t<M, N> solve(M a, N b, const long long &m) {
-    static_assert(std::is_integral_v<M>,
-                  "integer::solve argument must be integers.");
-    static_assert(std::is_integral_v<N>,
-                  "integer::solve argument must be integers.");
+namespace congruence {
+/**
+ *  @brief Solve congruence ax = b (mod m) for all possible x.
+ *  @param a An integer from ax = b (mod m)
+ *  @param b An integer from ax = b (mod m)
+ *  @param m A modular value from ax = b (mod m)
+ */
+template <class M, class N, class T>
+constexpr std::vector<std::common_type_t<M, N>> solve(const M &a, const N &b, const T &m) {
+    static_assert(std::is_integral_v<M> && std::is_integral_v<N>,
+                  "integer::congruence::solve argument must be integers.");
+    static_assert(std::is_integral_v<T>,
+                  "integer::congruence::solve argument must be integers.");
+
+    std::vector<std::common_type_t<M, N>> ret;
 
     auto [g, u, v] = integer::gcd(a, m);
-    if (b % g) return 0;
 
-    return u * b / g;
+    if (b % g)
+        throw exceptions::InvalidSolutionException(
+            "There is no possible solution of given congruence.");
+
+    std::common_type_t<M, N> x0 = u * b / g;
+
+    for (int i = 0; i < g; ++i) {
+        ret.push_back(x0);
+        x0 += m / g;
+        x0 %= m;
+    }
+
+    return ret;
 }
-
+}  // namespace congruence
 }  // namespace integer
 
 #endif  // congruence.h
